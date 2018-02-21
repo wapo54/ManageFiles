8c55c43531e54e86d4923a678bc555b134ecfc3e
margarita itaj <itaimargi@hotmail.com>
2018-02-21 13:41:00 +0100

    doc

---
+++ db/migrate/20180221123659_create_docs.rb
@@ -0,0 +1,11 @@
+class CreateDocs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :docs do |t|
+      t.string :title
+      t.string :string
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
