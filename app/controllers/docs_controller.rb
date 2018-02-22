9d7db815c25a39b374bfe0e96bcd5da1201611e9
margarita itaj <itaimargi@hotmail.com>
2018-02-22 09:20:49 +0100

    form

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,16 +1,29 @@
 class DocsController < ApplicationController
 
   def show
-  # ...
   end
+
+
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+     render 'new'
+    end
+  end
+
   def edit
-  # ...
   end
+
   def update
-  # ...
   end
+
   def destroy
-  # ...
   end
 
   private
@@ -19,24 +32,6 @@ class DocsController < ApplicationController
   end
 
   def doc_params
+    params.require(:doc).permit(:title, :content)
   end
 end
-
-def new
-   @doc = Doc.new
-  end
-
-  def create
-   @doc = Doc.new(doc_params)
-   if @doc.save
-    redirect_to @doc
-  else
-   render 'new'
-  end
-end
-
-private
-
-  def doc_params
-    params.require(:doc).permit(.title, :content)
-  end
