40a76d72ee6b7eadd8bea243abedab3386bb342f
margarita itaj <itaimargi@hotmail.com>
2018-02-22 12:07:36 +0100

    sign up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -53,4 +55,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
