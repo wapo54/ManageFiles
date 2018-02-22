b34cc44e4a88c853a93a6e680660557f8c1dd888
margarita itaj <itaimargi@hotmail.com>
2018-02-22 15:46:02 +0100

    add views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,7 +3,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.all
+    @docs = Doc.where(user_id: current_user)
   end
 
   def show
@@ -11,11 +11,11 @@ class DocsController < ApplicationController
   #...
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs:build
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save
       redirect_to @doc
     else
