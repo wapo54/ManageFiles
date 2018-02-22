ce0de8d79cb343241dacbf51e9fcb26e9bc45b83
margarita itaj <itaimargi@hotmail.com>
2018-02-22 10:52:28 +0100

    new doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,8 +1,14 @@
 class DocsController < ApplicationController
 
-  def show
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @docs = Doc.all(“created_at_DESC”)
   end
 
+  def show
+  end
+  #...
 
   def new
     @doc = Doc.new
@@ -21,17 +27,26 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+  @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
   def find_doc
+    @doc = Doc.find(params[:id])
   end
 
   def doc_params
     params.require(:doc).permit(:title, :content)
   end
 end
+
