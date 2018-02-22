b6bb6f0b5d42af37fd009f115841b361f7604952
margarita itaj <itaimargi@hotmail.com>
2018-02-22 11:26:29 +0100

    fix it

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -3,7 +3,7 @@ class DocsController < ApplicationController
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
-    @docs = Doc.all(“created_at_DESC”)
+    @docs = Doc.all
   end
 
   def show
